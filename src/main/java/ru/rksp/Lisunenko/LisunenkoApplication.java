40925eb815df30f6485a2d4a4b8aade45f0b01fb
Manhyato <145689933+Manhyato@users.noreply.github.com>
2025-10-17 12:54:47 +0300

    Initial commit

---
+++ src/main/java/ru/rksp/Lisunenko/LisunenkoApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Lisunenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisunenkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisunenkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/Lisunenko/LisunenkoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.Lisunenko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisunenkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
